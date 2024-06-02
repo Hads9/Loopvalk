b4cc13eb4f293ce5796b31b00060c90d772ed456
Hads9 <pjacinto1249@gmail.com>
2024-06-02 13:32:36 -0300

    .lua

---
+++ .lua
@@ -0,0 +1,18 @@
+--ppk
+
+_G.print = true
+
+--krlh
+
+while true do
+wait(1.0)
+if _G.print == true then
+print("Loop executado com sucesso")
+    local args = {
+    [1] = "wear",
+    [2] = 17472943028
+}
+
+game:GetService("ReplicatedStorage").RE:FindFirstChild("1Updat1eAvata1r"):FireServer(unpack(args))
+    end
+end
